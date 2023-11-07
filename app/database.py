1488f6a1bc7d16b6ca25dace42a63fc8cc68475d
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-11-07 19:36:21 +0300

    add black

--- app/database.py
+++ app/database.py
@@ -6,12 +6,12 @@ import os
 
 load_dotenv()
 
+
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
-
 
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
@@ -21,4 +21,3 @@ engine = create_async_engine(DATABASE_URL)
 
 class Base(DeclarativeBase):
     pass
-
