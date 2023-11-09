999dfa38a1dca5e45ad62518cf1504215d9e6a69
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-11-09 12:29:01 +0300

    created dependencies

---
+++ api_v1/user/dependencies.py
@@ -0,0 +1,20 @@
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Annotated
+from . import crud
+from core.models import db_helper, User
+from fastapi import Path
+
+
+async def user_by_id(
+    user_id: Annotated[int, Path],
+    session: AsyncSession = Depends(db_helper.session_dependency),
+) -> User:
+    user = await crud.get_user_by_id(user_id=user_id, session=session)
+    if user:
+        return user
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"User with id {user_id} is not found",
+    )
