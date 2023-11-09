cb51ba59d44f7aa6d0359542dc720eef72dde87d
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-11-09 10:09:16 +0300

    added check for user with existing email

--- api_v1/user/views.py
+++ api_v1/user/views.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from . import crud
 from .schemas import UserSchema, UserCreateSchema
 from core.models import db_helper
+from asyncpg.exceptions import UniqueViolationError
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -14,11 +15,19 @@ async def get_users(
     return await crud.get_users(session=session)
 
 
-@router.post("/", response_model=UserSchema)
+# @router.post("/", response_model=UserSchema)
+@router.post("/")
 async def create_user(
     user: UserCreateSchema,
     session: AsyncSession = Depends(db_helper.session_dependency),
 ):
+    db_user = crud.get_user_by_email(session=session, email=user.email)
+    if db_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Probably user with this email is already exist",
+        )
+
     return await crud.add_user(session=session, user=user)
 
 
@@ -26,7 +35,7 @@ async def create_user(
 async def get_user(
     user_id: int, session: AsyncSession = Depends(db_helper.session_dependency)
 ):
-    user = await crud.get_user(user_id=user_id, session=session)
+    user = await crud.get_user_by_id(user_id=user_id, session=session)
     if user:
         return user
 
