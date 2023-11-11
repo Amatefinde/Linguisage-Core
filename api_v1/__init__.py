61fd7db3e01aae989c2a64b28db37d70aebcff4e
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-11-11 12:11:00 +0300

    include literature router

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 
 from .user.views import router as user_router
+from .literature.views import router as literature_router
+
 
 router = APIRouter()
 router.include_router(user_router)
+router.include_router(literature_router)
