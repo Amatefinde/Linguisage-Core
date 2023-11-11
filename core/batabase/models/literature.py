51dde436c9b598ca6e4c441f29bf11322147d0c4
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-11-11 11:17:28 +0300

    add literature table

--- core/batabase/models/literature.py
+++ core/batabase/models/literature.py
@@ -1,5 +1,19 @@
 from core.batabase.base import Base
 
+from sqlalchemy import TIMESTAMP, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
 
-# class Literature(Base):
-#
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .user import User
+
+
+class Literature(Base):
+    title: Mapped[str]
+    content: Mapped[int] = mapped_column(unique=True)
+    add_datetime: Mapped[datetime] = mapped_column(TIMESTAMP)
+    last_open_datetime: Mapped[datetime | None] = mapped_column(TIMESTAMP)
+
+    user: Mapped["User"] = mapped_column(ForeignKey("user_account.id"))
