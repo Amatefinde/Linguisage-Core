7237dbeb3881492169771b2387e4b89925eb5dfd
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-11-11 20:01:43 +0300

    user session add function

--- api_v1/user/crud.py
+++ api_v1/user/crud.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from core.database.models import User
+from core.database.models import User, Session
 from sqlalchemy.engine import Result
-from .schemas import UserCreateSchema, UserUpdateSchema
+from .schemas import UserCreateSchema, UserUpdateSchema, SessionSchema
 from ..auth_tools.auth import get_hashed_password
 
 
@@ -55,4 +55,38 @@ async def update_user_partition(
 
 async def delete_user(session: AsyncSession, user: User):
     await session.delete(user)
+    await session.commit()
+
+
+async def add_user_session(session: AsyncSession, user_session: SessionSchema):
+    user = await get_user_by_id(session=session, user_id=user_session.user_id)
+
+    stmt = (
+        select(Session)
+        .where(Session.user_id == user_session.user_id)
+        .where(Session.ip == user_session.ip)
+        .where(Session.browser_header == user_session.browser_header)
+    )
+    db_session = await session.scalar(stmt)
+    if db_session:
+        db_session.token = user_session.token
+        await session.commit()
+        return await session.refresh(db_session)
+
+    db_session = Session(**user_session.model_dump(), user=user)
+    session.add(db_session)
+    await session.commit()
+    await session.refresh(db_session)
+
+    print(
+        "Вход с нового устройства: ", *list(user_session.model_dump().items()), sep="\n"
+    )
+
+    return db_session
+
+
+async def delete_user_session(session: AsyncSession, user_session: SessionSchema):
+    user = await get_user_by_id(session=session, user_id=user_session.user_id)
+    db_session = Session(**user_session.model_dump(), user=user)
+    await session.delete(db_session)
     await session.commit()
