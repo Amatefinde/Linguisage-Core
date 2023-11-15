052d317a9e49d4a03f0c6deb272f760693fc0242
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-11-15 17:56:20 +0300

    add cors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from core.config import settings
 from api_v1 import router
@@ -16,6 +17,19 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router, prefix=settings.api_v1_prefix)
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8080",
+    "http://192.168.31.23:7210",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
