c4df3a1946803d50c534065fe85d089b6653eb5d
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-11-16 14:56:45 +0300

    add WordMeaning table

---
+++ core/database/models/word_meaning.py
@@ -0,0 +1,21 @@
+from core.database import Base
+from sqlalchemy import Enum, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import Literal, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from . import User, Literature
+
+status = Enum("complete", "in_process", "in_queue")
+
+
+class WordMeaning(Base):
+    content: Mapped[str]
+    status: Mapped[str] = mapped_column(status)
+    literature_id: Mapped[int] = mapped_column(
+        ForeignKey="literature.id", nullable=True
+    )
+    user_id: Mapped[int] = mapped_column(ForeignKey="user.id")
+
+    literature: Mapped["Literature"] = relationship(back_populates="words")
+    user: Mapped["User"] = relationship(back_populates="words")
