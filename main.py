3e60ad13f338e4e3346d11951c91d9d466c42215
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-11-20 16:37:22 +0300

    add more cors

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, Header
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from starlette.datastructures import Headers
+from starlette.requests import Request
+
 from core.config import settings
 from api_v1 import router
 
@@ -16,20 +19,58 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-app.include_router(router, prefix=settings.api_v1_prefix)
+
+
+@app.middleware("")
+async def add_cors_header(request: Request, call_next):
+    print(request.__dict__)
+    # if "Origin" not in request.headers:
+    #     response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
+    response = await call_next(request)
+    return response
+
+
 origins = [
+    "http://192.168.31.23:3000",
+    "http://93.81.252.160:8001",
+    "http://93.81.252.160:7777",
+    "http://93.81.252.160:7777/",
+    "http://93.81.252.160:3000",
     "http://localhost:3000",
-    "http://localhost:8080",
-    "http://192.168.31.23:7210",
+    "http://192.168.31.1:0000",
+    "http://26.96.4.216:3000",
+    "http://linguitest.ddns.net",
+    "http://linguitest.ddns.net:7777",
 ]
+
+headers = [
+    "Authorization",
+    "Content-Type",
+    "Set-Cookie",
+    "Accept",
+    "Content-Length",
+    "User-Agent",
+    "Proxy-Authorization",
+    "Proxy-Connection",
+    "User-Agent",
+    "Access-Control-Allow-Headers",
+    "Access-Control-Request-Method",
+    "Access-Control-Allow-Origin",
+]
+
+method = ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"]
+
+app.include_router(router, prefix=settings.api_v1_prefix)
+
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allow_headers=headers,
 )
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
