87c7488828798556e5d15d66557eedebbbb46f7a
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-11-22 22:15:47 +0300

    interaction with other microservices now async

--- api_v1/literature/crud.py
+++ api_v1/literature/crud.py
@@ -1,7 +1,13 @@
+import asyncio
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+
+from core.config import settings
 from core.database.models import Literature
 from datetime import datetime
+import aiohttp
+from .schemas import LiteratureResponseScheme
 
 
 async def get_literature_by_id(
@@ -12,14 +18,43 @@ async def get_literature_by_id(
     return literature_db
 
 
+async def get_literature_cover(session, literature_id):
+    params = {
+        "literature_id": literature_id,
+        "start_page": 1,
+        "end_page": 1,
+    }
+    url = settings.content_manager_url + "/literature/get_pages"
+    async with session.get(url, params=params) as resp:
+        try:
+            return await resp.json()
+        except Exception:
+            return None
+
+
 async def get_all_user_literature(
     session: AsyncSession,
     user_id: int,
-) -> list[Literature]:
+) -> list[LiteratureResponseScheme]:
     stmt = select(Literature).where(Literature.user_id == user_id)
+
     literature_db = await session.scalars(stmt)
-    literature_db = list(literature_db)
-    return literature_db
+    literatures = [LiteratureResponseScheme(**x.__dict__) for x in literature_db]
+    async with aiohttp.ClientSession() as session:
+        tasks = [
+            get_literature_cover(session, instance.content) for instance in literatures
+        ]
+
+        books = await asyncio.gather(*tasks)
+        covers = [
+            (x[0]["img"] if len(x) else None) if type(x) is list else None
+            for x in books
+        ]
+
+    for idx, literature in enumerate(literatures):
+        literature.cover = covers[idx]
+
+    return literatures
 
 
 async def add_literature_by_user_id(
--- core/providers/content_provider.py
+++ core/providers/content_provider.py
@@ -1,32 +1,47 @@
 import requests
 from os import path
 from core.config import settings
+import aiohttp
 
 
-def add_literature(file, use_ocr: bool = False) -> int | None:
-    files = {"file": ("image.jpg", file)}
+# async def add_literature(file, use_ocr: bool = False) -> int | None:
+#     files = {"file": ("image.jpg", file)}
+#     url = settings.content_manager_url + "/literature/add"
+#     response = requests.post(url, files=files, params={"use_ocr": str(use_ocr).lower()})
+#     if response.status_code != 200:
+#         return
+#     return int(response.json())
+
+
+async def add_literature(file, use_ocr: bool = False) -> int | None:
     url = settings.content_manager_url + "/literature/add"
-    response = requests.post(url, files=files, params={"use_ocr": str(use_ocr).lower()})
-    if response.status_code != 200:
-        return
-    return int(response.json())
+    data = aiohttp.FormData()
+    data.add_field("file", file, filename="image.jpg")
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            url, data=data, params={"use_ocr": str(use_ocr).lower()}
+        ) as response:
+            if response.status != 200:
+                return
+            return int(await response.json())
 
 
-def get_literature_pages(
+async def get_literature_pages(
     content_id: int, start_page: int = 1, end_page: int = 0
 ) -> dict:
     url = settings.content_manager_url + "/literature/get_pages"
-    response = requests.get(
-        url,
-        params={
-            "literature_id": content_id,
-            "start_page": start_page,
-            "end_page": end_page,
-        },
-    )
-    if response.status_code != 200:
-        return
-    return response.json()
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            url,
+            params={
+                "literature_id": content_id,
+                "start_page": start_page,
+                "end_page": end_page,
+            },
+        ) as response:
+            if response.status != 200:
+                return
+            return await response.json()
 
 
 def get_word_meaning(meaning_id: int) -> dict:
