89460f72924c0e322532912d183fd4dcbd0be7d2
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-11-22 22:16:38 +0300

    add field for return covers of books either

--- api_v1/literature/schemas.py
+++ api_v1/literature/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from datetime import datetime
 
 
@@ -8,3 +8,7 @@ class LiteratureResponseScheme(BaseModel):
     add_datetime: datetime
     last_open_datetime: datetime | None = None
     user_id: int
+    cover: str | None = None
+    content: int
+
+    model_config = ConfigDict(from_attributes=True)
