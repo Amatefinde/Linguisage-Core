5e0bb8c90806a626bb679e648668634f5b49baa1
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-12-05 08:56:13 +0300

    add schema for validate response by Dictionary Microservice

--- core/providers/Dictionary/schemas.py
+++ core/providers/Dictionary/schemas.py
@@ -34,4 +34,40 @@ class SenseDTO(BaseDTO):
 class WordDTO(BaseDTO):
     id: int
     word: str
+    current_sense_id: int | None = None
     senses: list[SenseDTO]
+
+
+###################################
+
+
+class SBaseP(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+
+class SExamplesP(SBaseP):
+    example: str
+
+
+class SRowExamplesP(SBaseP):
+    row_example: str
+
+
+class SImageP(SBaseP):
+    id: int
+    img: str
+
+
+class SWordForSenseP(SBaseP):
+    word: str
+
+
+class SSenseP(SBaseP):
+    lvl: Literal["A1", "A2", "B1", "B2", "C1", "C2"] | None = None
+    short_cut: str | None = None
+    definition: str
+    examples: list[SExamplesP] = None
+    row_examples: list[SRowExamplesP] = None
+    images: list[SImageP] = None
+
+    word: SWordForSenseP | None = None
