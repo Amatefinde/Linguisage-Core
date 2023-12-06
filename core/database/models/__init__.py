2341b32103184078adccd10d8d8d3a9fc824e79f
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-12-06 15:50:42 +0300

    add import answer model

--- core/database/models/__init__.py
+++ core/database/models/__init__.py
@@ -1,11 +1,6 @@
-__all__ = (
-    "User",
-    "Session",
-    "Literature",
-    "Sense",
-    "Image",
-)
+__all__ = ("User", "Session", "Literature", "Sense", "Image", "Answer")
 
 from .user import User, Session
 from .literature import Literature
 from .sense import Sense, Image
+from .training import Answer
