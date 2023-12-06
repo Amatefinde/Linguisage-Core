ea29bbe2d75cf45636aa2d70fb543e1e74d58286
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-12-06 15:53:26 +0300

    add created_at field for sense table

--- core/database/models/sense.py
+++ core/database/models/sense.py
@@ -1,16 +1,21 @@
+from datetime import datetime
+
 from core.database import Base
-from sqlalchemy import Enum, ForeignKey
+from sqlalchemy import Enum, ForeignKey, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import Literal, TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from . import User, Literature
+    from . import User, Literature, Answer
 
 status = Enum("complete", "in_process", "in_queue", name="word_status")
 
 
 class Sense(Base):
     f_sense_id: Mapped[int]
+    created_at: Mapped[datetime] = mapped_column(
+        default=datetime.utcnow(), server_default=func.now()
+    )
     status: Mapped[str] = mapped_column(status, default="in_queue")
 
     literature_id: Mapped[int] = mapped_column(
@@ -23,6 +28,7 @@ class Sense(Base):
     images: Mapped[list["Image"]] = relationship(
         back_populates="senses", cascade="all, delete-orphan"
     )
+    answers: Mapped[list["Answer"]] = relationship(back_populates="sense")
 
 
 class Image(Base):
