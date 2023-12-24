35f6434814cf2a34038f902a53f147d1efc87548
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2023-12-24 19:35:18 +0300

    import train exercises

--- api_v1/training/views.py
+++ api_v1/training/views.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, TYPE_CHECKING
 
 from fastapi import APIRouter, Depends, Query
 from pydantic import Field
@@ -9,6 +9,9 @@ from core.database.models import User
 from api_v1.user import get_current_user
 from . import crud
 from .scemas import SAnswer
+
+if TYPE_CHECKING:
+    from core.providers.Dictionary.schemas import SSenseP
 
 router = APIRouter(prefix="/training", tags=["training"])
 
@@ -23,7 +26,13 @@ async def get_training(
     session: AsyncSession = Depends(db_helper.session_dependency),
 ):
     await train_generator.define_mastered_senses(session, user)
-    return await train_generator.generate(session, user, number)
+    bunch_train_content: list[SSenseP] = await train_generator.generate(session, user, number)
+    for train_content in bunch_train_content:
+        word = train_content.word.word
+        for example in train_content.row_examples:
+            ready_row_example = example.row_example.replace(word, "_"*len(word))
+            example.row_example = ready_row_example
+    return bunch_train_content
 
 
 @router.post(
