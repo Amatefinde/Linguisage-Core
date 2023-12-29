680c685fc492d1b43bfc6745fe37026c0b04caa9
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2023-12-29 10:43:56 +0300

    fix score less them 0

--- api_v1/train_logic/train_logic.py
+++ api_v1/train_logic/train_logic.py
@@ -18,9 +18,11 @@ async def calculate_score_by_sense_with_answers(
 
     for sense_with_answer in sense_with_answers[0].answers:
         if sense_with_answer.is_correct:
-            score += 0.25
+            score += 0.20
         else:
             score -= 0.1
+            if score < 0:
+                score = 0
     return score
 
 
@@ -28,11 +30,7 @@ async def get_score_and_status_by_f_sense_id(
     session: AsyncSession,
     f_sense_id,
 ) -> ScoreAndStatus:
-    stmt = (
-        select(Sense)
-        .where(Sense.f_sense_id == f_sense_id)
-        .options(selectinload(Sense.answers))
-    )
+    stmt = select(Sense).where(Sense.f_sense_id == f_sense_id).options(selectinload(Sense.answers))
     db_response = await session.execute(stmt)
     sense_with_ans = db_response.scalars().all()
     status = sense_with_ans[0].status
