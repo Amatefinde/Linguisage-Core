df5cf472687467055b80b6eef60a964f44f427e5
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-03-01 00:10:36 +0300

    fix last verified time after verify request

--- src/api_v1/user/crud.py
+++ src/api_v1/user/crud.py
@@ -1,6 +1,11 @@
 from datetime import datetime
 
-from sqlalchemy.orm import Session
-
-
+from src.core.database import db_helper
 from src.core.database.models import User
+
+
+async def set_user_last_verified_by_now(user: User):
+    async with db_helper.session_factory() as session:
+        db_user = await session.get(User, user.id)
+        db_user.last_verification_request = datetime.utcnow()
+        await session.commit()
