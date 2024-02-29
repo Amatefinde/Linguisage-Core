5da151b3bd4f5ce33627bbad29b360cab2312535
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-02-29 19:05:24 +0300

    add account_status field

--- src/api_v1/user/schemas.py
+++ src/api_v1/user/schemas.py
@@ -4,16 +4,22 @@ from datetime import datetime
 from fastapi_users import schemas
 from pydantic import Field, EmailStr
 
-
-class UserRead(schemas.BaseUser[uuid.UUID]):
-    last_verification_request: datetime | None = None
+from src.core import types
 
 
-class UserCreate(schemas.CreateUpdateDictModel):
+class UserGeneralFieldMixin(schemas.CreateUpdateDictModel):
     username: str
+
+
+class UserRead(schemas.BaseUser[uuid.UUID], UserGeneralFieldMixin):
+    last_verification_request: datetime | None = None
+    account_status: types.account_status
+
+
+class UserCreate(UserGeneralFieldMixin):
     email: EmailStr
     password: str
 
 
-class UserUpdate(schemas.BaseUserUpdate):
+class UserUpdate(schemas.BaseUserUpdate, UserGeneralFieldMixin):
     pass
