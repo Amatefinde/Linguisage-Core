0391a2f0fee3113bbf59b13c9a1f69f41fabf7e6
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-02-29 19:04:25 +0300

    add views router

--- src/api_v1/user/__init__.py
+++ src/api_v1/user/__init__.py
@@ -1,9 +1,10 @@
 __all__ = ["router"]
 
 
-from fastapi import APIRouter
-from .user_manager import fastapi_users, auth_backend
+from fastapi import APIRouter, Depends
+from .user_manager import fastapi_users, auth_backend, current_active_user_dependency
 from .schemas import UserCreate, UserRead, UserUpdate
+from .views import router as views_router
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
@@ -11,3 +12,4 @@ router.include_router(fastapi_users.get_auth_router(auth_backend))
 router.include_router(fastapi_users.get_register_router(UserRead, UserCreate))
 router.include_router(fastapi_users.get_reset_password_router())
 router.include_router(fastapi_users.get_verify_router(UserRead))
+router.include_router(views_router)
