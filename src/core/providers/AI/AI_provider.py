b9360f183df9981c3621dd731ac42e6cf1b82c43
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-04-01 14:57:40 +0300

    remove debug logs fix timeout for httpx

--- src/core/providers/AI/AI_provider.py
+++ src/core/providers/AI/AI_provider.py
@@ -11,9 +11,11 @@ from .schemas import RequestSenseReview, SenseReview
 async def review(request_sense_review: RequestSenseReview) -> SenseReview:
     async with AsyncClient() as httpx_client:
         url = urljoin(settings.ms.AI_MS_URL, "/review-sentence")
-        logger.debug(request_sense_review.model_dump())
-        response = await httpx_client.post(url, json=request_sense_review.model_dump())
-        logger.debug(response.json())
+
+        response = await httpx_client.post(
+            url, json=request_sense_review.model_dump(), timeout=10.0
+        )
+
         if response.status_code == 200:
             return SenseReview.model_validate(response.json())
         raise HTTPException(
